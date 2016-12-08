bcf01de2f267eb87243a75c07f280324c70c4e54
shines77 <gz_shines@msn.com>
2016-12-08 19:01:29 +0800

    Changes: Added atomic_padding.hpp;
    Signed-off-by: shines77 <gz_shines@msn.com>

---
+++ src/main/jimi_http_serv/atomic_padding.hpp
@@ -0,0 +1,185 @@
+
+#pragma once
+
+#include <atomic>
+#include <type_traits>
+
+#ifndef CACHE_LINE_SIZE
+#define CACHE_LINE_SIZE  64
+#endif
+
+#pragma pack(push)
+#pragma pack(1)
+
+namespace jimi {
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+namespace detail {
+
+//
+// See: http://en.cppreference.com/w/cpp/experimental/type_trait_variable_templates
+// See: http://en.cppreference.com/w/cpp/types/is_arithmetic
+// See: http://en.cppreference.com/w/cpp/types/is_class
+//
+template <typename T, bool isArithmetic = false>
+struct is_inheritable : std::integral_constant<bool,
+                        std::is_class<T>::value  &&
+#if (defined(__cplusplus) && (__cplusplus >= 201300L)) || (defined(_MSC_VER) && (_MSC_VER >= 1900L))
+                        !std::is_final<T>::value &&
+#endif // std::is_final<T> only can use in C++ 14.
+                        !std::is_volatile<T>::value> {};
+
+} // namespace detail
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+struct base_padding_data {};
+
+template <typename T>
+struct is_padding_data
+{
+    enum { value = std::is_base_of<T, base_padding_data>::value };
+};
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename T, std::size_t CacheLineSize, bool isInheritable = true>
+struct alignas(CacheLineSize)
+padding_data_impl : public T, public base_padding_data
+{
+    typedef T value_type;
+
+    static const std::size_t kCacheLineSize = CacheLineSize;
+    static const std::size_t kSizeofData = sizeof(value_type);
+    static const std::size_t kPaddingBytes =
+        (kCacheLineSize - kSizeofData) > 0 ? (kCacheLineSize - kSizeofData) : 1;
+
+    // Cacheline padding
+    char padding[kPaddingBytes];
+
+    padding_data_impl(value_type value) : T(value) {};
+    ~padding_data_impl() {};
+};
+
+template <typename T, std::size_t CacheLineSize>
+struct padding_data_impl<T, CacheLineSize, false> : public base_padding_data
+{
+    typedef typename std::remove_volatile<T>::type _T;
+    typedef _T value_type;
+
+    static const std::size_t kCacheLineSize = CacheLineSize;
+    static const std::size_t kSizeofData = sizeof(value_type);
+    static const std::size_t kPaddingBytes =
+        (kCacheLineSize - kSizeofData) > 0 ? (kCacheLineSize - kSizeofData) : 1;
+
+    // T aligned to cacheline size
+    alignas(CacheLineSize) value_type data;
+
+    // Cacheline padding
+    char padding[kPaddingBytes];
+
+    padding_data_impl(value_type value) : data(value) {};
+    ~padding_data_impl() {};
+};
+
+template <typename T, std::size_t CacheLineSize = CACHE_LINE_SIZE>
+struct padding_data : public padding_data_impl<
+                             T, CacheLineSize, detail::is_inheritable<T>::value> {};
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename T, std::size_t CacheLineSize, bool isInheritable = true>
+struct alignas(CacheLineSize)
+volatile_padding_data_impl : public T, public base_padding_data
+{
+    typedef typename std::remove_volatile<T>::type _T;
+    typedef volatile _T value_type;
+
+    static const std::size_t kCacheLineSize = CacheLineSize;
+    static const std::size_t kSizeofData = sizeof(value_type);
+    static const std::size_t kPaddingBytes =
+        (kCacheLineSize - kSizeofData) > 0 ? (kCacheLineSize - kSizeofData) : 1;
+
+    // Cacheline padding
+    char padding[kPaddingBytes];
+
+    volatile_padding_data_impl(_T value) : T(value) {};
+    ~volatile_padding_data_impl() {};
+};
+
+template <typename T, std::size_t CacheLineSize>
+struct volatile_padding_data_impl<T, CacheLineSize, false> : public base_padding_data
+{
+    typedef typename std::remove_volatile<T>::type _T;
+    typedef volatile _T value_type;
+
+    static const std::size_t kCacheLineSize = CacheLineSize;
+    static const std::size_t kSizeofData = sizeof(value_type);
+    static const std::size_t kPaddingBytes =
+        (kCacheLineSize - kSizeofData) > 0 ? (kCacheLineSize - kSizeofData) : 1;
+
+    // (volatile T) aligned to cacheline size
+    alignas(CacheLineSize) value_type data;
+
+    // Cacheline padding
+    char padding[kPaddingBytes];
+
+    volatile_padding_data_impl(value_type value) : data(value) {};
+    ~volatile_padding_data_impl() {};
+};
+
+template <typename T, std::size_t CacheLineSize = CACHE_LINE_SIZE>
+struct volatile_padding_data : public volatile_padding_data_impl<
+                                      T, CacheLineSize, detail::is_inheritable<T>::value> {};
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename T, std::size_t CacheLineSize = CACHE_LINE_SIZE>
+struct alignas(CacheLineSize)
+atomic_padding : public std::atomic<typename std::remove_volatile<T>::type>,
+                 public base_padding_data
+{
+    typedef typename std::remove_volatile<T>::type _T;
+    typedef std::atomic<_T> value_type;
+
+    static const std::size_t kCacheLineSize = CacheLineSize;
+    static const std::size_t kSizeofData = sizeof(value_type);
+    static const std::size_t kPaddingBytes =
+        (kCacheLineSize - kSizeofData) > 0 ? (kCacheLineSize - kSizeofData) : 1;
+
+    // Cacheline padding
+    char padding[kPaddingBytes];
+
+    atomic_padding(_T val) : std::atomic<typename std::remove_volatile<T>::type>(val) {};
+    ~atomic_padding() {};
+};
+
+template <typename T, std::size_t CacheLineSize = CACHE_LINE_SIZE>
+struct atomic_padding_wrapper : public base_padding_data
+{
+    typedef typename std::remove_volatile<T>::type _T;
+    typedef std::atomic<_T> value_type;
+
+    static const std::size_t kCacheLineSize = CacheLineSize;
+    static const std::size_t kSizeofData = sizeof(value_type);
+    static const std::size_t kPaddingBytes =
+        (kCacheLineSize - kSizeofData) > 0 ? (kCacheLineSize - kSizeofData) : 1;
+
+    // std::atomic<T> aligned to cacheline size
+    alignas(CacheLineSize) value_type data;
+
+    // Cacheline padding
+    char padding[kPaddingBytes];
+
+    atomic_padding_wrapper(_T value) : data(value) {};
+    ~atomic_padding_wrapper() {};
+};
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+} // namespace jimi
+
+#pragma pack(pop)
+
+#undef CACHELINE_SIZE
--- src/main/jimi_http_serv/main.cpp
+++ src/main/jimi_http_serv/main.cpp
@@ -9,6 +9,56 @@
 #include <boost/program_options.hpp>
 
 #include "utils.hpp"
+#include "atomic_padding.hpp"
+
+#define DEFAULT_PACKET_SIZE 32
+#define MAX_PACKET_SIZE     (128 * 1024)
+
+enum http_server_mode_t {
+    mode_http_server,
+    mode_echo_server,
+};
+
+std::string g_server_ip;
+std::string g_server_port;
+
+uint32_t g_mode         = http_server_mode_t::mode_http_server;
+uint32_t g_nodelay      = 0;
+uint32_t g_need_echo    = 1;
+uint32_t g_packet_size  = 64;
+
+std::string g_mode_str      = "echo";
+std::string g_nodelay_str   = "false";
+
+namespace jimi {
+
+extern atomic_padding<uint64_t> g_query_count;
+extern atomic_padding<uint32_t> g_client_count;
+
+extern atomic_padding<uint64_t> g_recv_bytes;
+extern atomic_padding<uint64_t> g_send_bytes;
+
+} // namespace jimi
+
+jimi::atomic_padding<uint64_t> jimi::g_query_count(0);
+jimi::atomic_padding<uint32_t> jimi::g_client_count(0);
+
+jimi::atomic_padding<uint64_t> jimi::g_recv_bytes(0);
+jimi::atomic_padding<uint64_t> jimi::g_send_bytes(0);
+
+void run_http_server(const std::string & host, const std::string & port,
+                     uint32_t packet_size, uint32_t thread_num,
+                     bool confirm = false)
+{
+    //
+}
+
+void run_http_server_ex(const std::string & host, const std::string & port,
+                        uint32_t packet_size, uint32_t thread_num,
+                        bool confirm = false)
+{
+    //
+}
 
 void make_spaces(std::string & spaces, std::size_t size)
 {
@@ -40,9 +90,10 @@ void print_usage(const std::string & app_name, const boost::program_options::opt
 int main(int argc, char * argv[])
 {
     std::string app_name;
-    std::string test_mode, test_method, nodelay, rpc_topic;
     std::string server_ip, server_port;
-    std::string mode, test, cmd, cmd_value;
+    std::string test_mode, test_method, nodelay, rpc_topic;
+    std::string mode_str, test_str, cmd, cmd_value;
+    int32_t mode = 0;
     int32_t pipeline = 1, packet_size = 0, thread_num = 0, need_echo = 1;
 
     namespace options = boost::program_options;
@@ -79,5 +130,107 @@ int main(int argc, char * argv[])
         exit(EXIT_FAILURE);
     }
 
+    // host
+    if (args_map.count("host") > 0) {
+        server_ip = args_map["host"].as<std::string>();
+    }
+    std::cout << "host: " << server_ip.c_str() << std::endl;
+    if (!is_valid_ip_v4(server_ip)) {
+        std::cerr << "Error: ip address \"" << server_ip.c_str() << "\" format is wrong." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // port
+    if (args_map.count("port") > 0) {
+        server_port = args_map["port"].as<std::string>();
+    }
+    std::cout << "port: " << server_port.c_str() << std::endl;
+    if (!is_socket_port(server_port)) {
+        std::cerr << "Error: port [" << server_port.c_str() << "] number must be range in (0, 65535]." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // mode
+    if (args_map.count("mode") > 0) {
+        mode_str = args_map["mode"].as<std::string>();
+    }
+
+    // Get the mode info
+    if (mode_str == "http") {
+        g_mode = mode_http_server;
+        g_mode_str = "Http Server";
+    }
+    if (mode_str == "echo") {
+        g_mode = mode_echo_server;
+        g_mode_str = "Http Echo Server";
+    }
+    else {
+        g_mode = mode_http_server;
+        g_mode_str = "Http Server";
+    }
+    std::cout << "mode string: " << mode_str.c_str() << std::endl;
+    std::cout << "mode       : " << g_mode_str.c_str() << std::endl;
+
+    // packet-size
+    if (args_map.count("packet-size") > 0) {
+        packet_size = args_map["packet-size"].as<int32_t>();
+    }
+    std::cout << "packet-size: " << packet_size << std::endl;
+    if (packet_size <= 0)
+        packet_size = DEFAULT_PACKET_SIZE;
+    if (packet_size > MAX_PACKET_SIZE) {
+        packet_size = MAX_PACKET_SIZE;
+        std::cerr << "Warnning: packet_size = " << packet_size << " can not set to more than "
+                  << MAX_PACKET_SIZE << " bytes [MAX_PACKET_SIZE]." << std::endl;
+    }
+    g_packet_size = packet_size;
+
+    // thread-num
+    if (args_map.count("thread-num") > 0) {
+        thread_num = args_map["thread-num"].as<int32_t>();
+    }
+    std::cout << "thread-num: " << thread_num << std::endl;
+    if (thread_num <= 0) {
+        thread_num = std::thread::hardware_concurrency();
+        std::cout << ">>> thread-num: std::thread::hardware_concurrency() = " << thread_num << std::endl;
+    }
+
+    // nodelay
+    if (args_map.count("nodelay") > 0) {
+        nodelay = args_map["nodelay"].as<std::string>();
+    }
+    if (nodelay == "1" || nodelay == "true") {
+        g_nodelay = 1;
+        g_nodelay_str = "true";
+    }
+    else {
+        g_nodelay = 0;
+        g_nodelay_str = "false";
+    }
+    std::cout << "TCP scoket no-delay: " << g_nodelay_str.c_str() << std::endl;
+
+    // Run the server
+    std::cout << std::endl;
+    std::cout << app_name.c_str() << " begin ..." << std::endl;
+    std::cout << std::endl;
+    std::cout << "listen " << server_ip.c_str() << ":" << server_port.c_str() << std::endl;
+    std::cout << "mode: " << mode_str.c_str() << std::endl;
+    std::cout << "packet_size: " << packet_size << ", thread_num: " << thread_num << std::endl;
+    std::cout << std::endl;
+
+    if (mode == mode_http_server) {
+        run_http_server(server_ip, server_port, packet_size, thread_num);
+    }
+    else if (mode == mode_echo_server) {
+        // TODO:
+        std::cout << "TODO: mode_echo_server." << std::endl;
+    }
+    else {
+        run_http_server_ex(server_ip, server_port, packet_size, thread_num);
+    }
+
+#ifdef _WIN32
+    ::system("pause");
+#endif
     return 0;
 }
