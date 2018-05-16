b44c4a32870c0532e487f08a8b964e276b827498
shines77 <gz_shines@msn.com>
2018-05-16 21:52:49 +0800

    改进和完善了StringRef<T>类;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/main/jimi/StringRef.h
+++ src/main/jimi/StringRef.h
@@ -13,131 +13,187 @@
 #include "jimi/basic/stddef.h"
 
 namespace jimi {
+
 namespace detail {
 
-    //////////////////////////////////////////
-    // function strlen<T>()
-    //////////////////////////////////////////
+//////////////////////////////////////////
+// detail::strlen<T>()
+//////////////////////////////////////////
 
-    template <typename CharType>
-    std::size_t strlen(CharType * str) {
-        return 0;
-    }
+template <typename CharType>
+inline std::size_t strlen(CharType * str) {
+    return (std::size_t)::strlen(str);
+}
 
-    template <>
-    std::size_t strlen(const char * str) {
-        return ::strlen(str);
-    }
+template <>
+inline std::size_t strlen(const char * str) {
+    return (std::size_t)::strlen(str);
+}
 
-    template <>
-    std::size_t strlen(const uint16_t * str) {
-        return ::wcslen((const wchar_t *)str);
-    }
+#if defined(_WIN32) || defined(WIN32) || defined(OS_WINDOWS) || defined(__WINDOWS__)
+template <>
+inline std::size_t strlen(const uint16_t * str) {
+    return (std::size_t)::wcslen((const wchar_t *)str);
+}
+#endif // _WIN32
 
-    template <>
-    std::size_t strlen(const wchar_t * str) {
-        return ::wcslen(str);
-    }
-
-    //////////////////////////////////////////
+template <>
+inline std::size_t strlen(const wchar_t * str) {
+    return (std::size_t)::wcslen(str);
+}
 
 } // namespace detail
 
 template <typename CharType>
 class BasicStringRef {
 public:
-    typedef CharType char_type;
-    typedef std::size_t size_type;
-    typedef std::basic_string<char_type> std_string;
+    typedef CharType            value_type;
+    typedef std::size_t         size_type;
+    typedef std::intptr_t       difference_type;
+    typedef CharType *          pointer;
+    typedef const CharType *    const_pointer;
+    typedef CharType &          reference;
+    typedef const CharType &    const_reference;
+
+    typedef std::basic_string<value_type> string_type;
+    typedef BasicStringRef<value_type> stringref_type;
 
 private:
-    const char_type * data_;
-    size_type size_;
+    const value_type * data_;
+    size_type length_;
 
 public:
-    BasicStringRef() : data_(nullptr), size_(0) {}
-    BasicStringRef(const char_type * data) : data_(data), size_(detail::strlen(data)) {}
-    BasicStringRef(const char_type * data, size_type size) : data_(data), size_(size) {}
+    BasicStringRef() : data_(nullptr), length_(0) {}
+    BasicStringRef(const value_type * data)
+        : data_(data), length_(detail::strlen(data)) {}
+    BasicStringRef(const value_type * data, size_type length)
+        : data_(data), length_(length) {}
+    BasicStringRef(const value_type * first, const value_type * last)
+        : data_(first), length_(size_type(last - first)) {}
     template <size_type N>
-    BasicStringRef(char_type (&src)[N]) : data_(src), size_(N) {}
-    BasicStringRef(const std_string & src) : data_(src.data()), size_(src.size()) {}
-    BasicStringRef(std_string && src) : data_(src.data()), size_(src.size()) {}
-    BasicStringRef & operator = (const std_string & rhs) {
-        this->data_ = rhs.data();
-        this->size_ = rhs.size();
-        return *this;
+    BasicStringRef(const value_type (&data)[N])
+        : data_(data), length_(N - 1) {}
+    BasicStringRef(const string_type & src)
+        : data_(src.c_str()), length_(src.size()) {}
+    BasicStringRef(const stringref_type & src)
+        : data_(src.data()), length_(src.length()) {}
+    BasicStringRef(string_type && src)
+        : data_(src.c_str()), length_(src.size()) {
+        // Only reference from src string
     }
-    BasicStringRef(const BasicStringRef & src) : data_(src.data()), size_(src.size()) {}
-    BasicStringRef(BasicStringRef && src) : data_(src.data()), size_(src.size()) {
+    BasicStringRef(stringref_type && src)
+        : data_(src.data()), length_(src.length()) {
         src.reset();
     }
-    BasicStringRef & operator = (const BasicStringRef & rhs) {
-        this->data_ = rhs.data();
-        this->size_ = rhs.size();
+    ~BasicStringRef() { /* Do nothing! */ }
+
+    BasicStringRef & operator = (const value_type * data) {
+        this->data_ = data;
+        this->length_ = detail::strlen(data);
         return *this;
     }
-    ~BasicStringRef() {}
 
-    const char_type * data() const { return data_; }
-    size_type size() const  { return size_; }
+    BasicStringRef & operator = (const string_type & rhs) {
+        this->data_ = rhs.c_str();
+        this->length_ = rhs.size();
+        return *this;
+    }
 
-    const char_type * c_str() const { return (data() != nullptr) ? data() : ""; }
-    size_type length() const { return size(); }
+    BasicStringRef & operator = (const stringref_type & rhs) {
+        this->data_ = rhs.data();
+        this->length_ = rhs.length();
+        return *this;
+    }
+    
+    const value_type * data() const { return this->data_; }
+    size_type length() const { return this->length_; }
 
-    bool empty() const { return (size() == 0); }
+    const value_type * c_str() const { return this->data(); }
+    size_type size() const  { return this->length(); }
+
+    bool empty() const { return (this->length() == 0); }
 
     void reset() {
-        data_ = nullptr;
-        size_ = 0;
+        this->data_ = nullptr;
+        this->length_ = 0;
     }
 
     void clear() {
-        data_ = "";
-        size_ = 0;
+        this->reset();
     }
 
-    void assign(const char_type * data) {
-        data_ = data;
-        size_ = ::strlen(data);
+    void assign(const value_type * data) {
+        this->set_data(data, detail::strlen(data));
     }
 
-    void assign(const char_type * data, size_type size) {
-        data_ = data;
-        size_ = size;
+    void assign(const value_type * data, size_type length) {
+        this->set_data(data, length);
     }
 
-    void assign(const std_string & src) {
-        data_ = src.data();
-        size_ = src.size();
+    void assign(const value_type * first, const value_type * last) {
+        this->set_data(first, size_type(last - first));
     }
 
     template <size_type N>
-    void assign(char_type (&src)[N]) {
-        data_ = src;
-        size_ = N;
+    void assign(const value_type (&src)[N]) {
+        this->set_data(data, N - 1);
+    }
+
+    void assign(const string_type & src) {
+        this->set_data(src.c_str(), src.size());
+    }
+
+    void assign(const stringref_type & src) {
+        this->set_data(src.data(), src.length());
+    }
+
+    void set_data(const value_type * data, size_type length) {
+        this->data_ = data;
+        this->length_ = length;
+    }
+
+    void set_data(const value_type * data) {
+        this->set_data(data, detail::strlen(data));
+    }
+
+    void set_data(const value_type * first, const value_type * last) {
+        this->set_data(first, size_type(last - first));
+    }
+
+    template <size_type N>
+    void set_data(const value_type (&src)[N]) {
+        this->set_data(data, N - 1);
+    }
+
+    void set_data(const string_type & src) {
+        this->set_data(src.c_str(), src.size());
+    }
+
+    void set_data(const stringref_type & src) {
+        this->set_data(src.data(), src.length());
     }
 
     std::string toString() const {
-        return std::string(data_, size_);
+        return std::string(this->data_, this->length_);
     }
 };
 
 template <typename CharType>
 class BasicStringRefHelper {
 public:
-    typedef CharType char_type;
-    typedef BasicStringRef<char_type> string_ref;
+    typedef CharType value_type;
+    typedef BasicStringRef<value_type> stringref_type;
 
 private:
     bool truncated_;
-    string_ref str_;
-    char_type save_char_;
+    stringref_type str_;
+    value_type save_char_;
 
 public:
     BasicStringRefHelper() : truncated_(false) { (void)save_char_; }
     ~BasicStringRefHelper() { detach(); }
 
-    bool attach(const string_ref & str) {
+    bool attach(const stringref_type & str) {
         // If the string reference don't recover the truncated char,
         // don't accept the new attach.
         if (likely(!truncated_)) {
@@ -157,19 +213,19 @@ public:
 
     void truncate() {
         if (likely(!truncated_)) {
-            char_type * first = (char_type *)str_.data();
-            char_type * last = first + str_.size();
+            value_type * first = (value_type *)str_.data();
+            value_type * last = first + str_.size();
             assert(last != nullptr);
             save_char_ = *last;
-            *last = static_cast<char_type>('\0');
+            *last = static_cast<value_type>('\0');
             truncated_ = true;
         }
     }
 
     void recover() {
         if (likely(truncated_)) {
-            char_type * first = (char_type *)str_.data();
-            char_type * last = first + str_.size();
+            value_type * first = (value_type *)str_.data();
+            value_type * last = first + str_.size();
             assert(last != nullptr);
             *last = save_char_;
             truncated_ = false;
