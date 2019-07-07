b5a64b4f2606fbf3fd12cc95741c108cdac1003a
shines77 <gz_shines@msn.com>
2019-07-08 01:27:17 +0800

    小改动;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/test/http_parser_test/main.cpp
+++ src/test/http_parser_test/main.cpp
@@ -2153,8 +2153,8 @@ void pico_http_parser_benchmark()
     do {
         /* Parse the request */
         num_headers = sizeof(headers) / sizeof(headers[0]);
-        pret = ::phr_parse_request(http_header, buflen, (const char **)&method, &method_len, (const char **)&path, &path_len,
-                                   &minor_version, headers, &num_headers, prevbuflen);
+        pret = phr_parse_request(http_header, buflen, (const char **)&method, &method_len, (const char **)&path, &path_len,
+                                 &minor_version, headers, &num_headers, prevbuflen);
         if (pret > 0) {
             /* successfully parsed the request */
             std::atomic_thread_fence(std::memory_order_acquire);
